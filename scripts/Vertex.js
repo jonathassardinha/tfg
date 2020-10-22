7c08bec488a7a35391fec1d241901d1d90492aef
Jonathas Sardinha <jsard@motorola.com>
2020-10-21 22:59:47 -0300

    Changed vertex from circle to round rectangle

--- scripts/Vertex.js
+++ scripts/Vertex.js
@@ -1,53 +1,58 @@
 (function() {
 
-  function Vertex(stage, circleColor, outlineColor, radius, maxRadius) {
+  function Vertex(stage, w, h, rectColor, outlineColor, radius) {
     this.Container_constructor();
 
     // this.stage = stage;
-    this.circleColor = circleColor;
+    this.w = w;
+    this.h = h;
+    this.rectColor = rectColor;
     this.outlineColor = outlineColor;
     this.radius = radius;
-    this.minRadius = this.radius;
-    this.maxRadius = maxRadius;
+    this.minW = this.w;
+    this.minH = this.h;
+    this.maxW = this.w + 3;
+    this.maxH = this.h + 3;
     this.isPressed = false;
-    this.rampSpace = (maxRadius-radius)/3;
+    this.rampSpaceW = (this.maxW-this.w)/3;
+    this.rampSpaceH = (this.maxH-this.h)/3;
     this.setup();
   }
   var p = createjs.extend(Vertex, createjs.Container);
 
   p.setup = function() {
-    this.circle = new createjs.Shape();
-    this.circle.graphics.beginFill(this.circleColor).drawCircle(0, 0, this.radius);
-    this.circle.cursor = "pointer";
-    this.circle.x = this.x;
-    this.circle.y = this.y;
+    this.rRect = new createjs.Shape();
+    this.rRect.graphics.beginFill(this.rectColor).drawRoundRect(-this.w/2, -this.h/2, this.w, this.h, this.radius);
+    this.rRect.cursor = "pointer";
+    this.rRect.x = this.x;
+    this.rRect.y = this.y;
 
     this.outline = new createjs.Shape();
-    this.outline.graphics.setStrokeStyle(2).beginStroke(this.outlineColor).drawCircle(0, 0, this.radius+5);
+    this.outline.graphics.setStrokeStyle(2).beginStroke(this.outlineColor).drawRoundRect(-(this.w/2+3.5), -(this.h/2+3.5), this.w+7, this.h+7, this.radius);
 
-    this.circle.on("mouseover", () => {
+    this.rRect.on("mouseover", () => {
       this.addChild(this.outline);
     });
 
-    this.circle.on("mouseout", () => {
+    this.rRect.on("mouseout", () => {
       if (!this.isPressed) this.removeChild(this.outline);
     });
 
-    this.circle.on("mousedown", () => {
+    this.rRect.on("mousedown", () => {
       this.isPressed = true;
-      if (this.circleEventListener) this.circle.off("tick", this.circleEventListener);
-      this.circleEventListener = this.circle.on("tick", this.handleMouseDown);
+      if (this.rRectEventListener) this.rRect.off("tick", this.rRectEventListener);
+      this.rRectEventListener = this.rRect.on("tick", this.handleMouseDown);
     });
 
-    this.circle.on("pressup", () => {
+    this.rRect.on("pressup", () => {
       this.isPressed = false;
-      var pt = this.circle.globalToLocal(this.stage.mouseX, this.stage.mouseY);
-      if (!this.circle.hitTest(pt.x, pt.y)) this.removeChild(this.outline);
-      if (this.circleEventListener) this.circle.off("tick", this.circleEventListener);
-      this.circleEventListener = this.circle.on("tick", this.handleMouseUp);
+      var pt = this.rRect.globalToLocal(this.stage.mouseX, this.stage.mouseY);
+      if (!this.rRect.hitTest(pt.x, pt.y)) this.removeChild(this.outline);
+      if (this.rRectEventListener) this.rRect.off("tick", this.rRectEventListener);
+      this.rRectEventListener = this.rRect.on("tick", this.handleMouseUp);
     })
 
-    this.addChild(this.circle);
+    this.addChild(this.rRect);
 
     // this.mouseChildren = false;
 
@@ -56,28 +61,30 @@
   };
 
   p.handleMouseDown = function() {
-    this.parent.radius += this.parent.rampSpace;
-    if (this.parent.radius >= this.parent.maxRadius) {
-      this.off("tick", this.parent.circleEventListener);
-      this.parent.circleEventListener = null;
-      this.parent.radius = this.parent.maxRadius;
+    this.parent.w += this.parent.rampSpaceW;
+    this.parent.h += this.parent.rampSpaceH;
+    if (this.parent.w >= this.parent.maxW || this.parent.h >= this.parent.maxH) {
+      this.off("tick", this.parent.rRectEventListener);
+      this.parent.rRectEventListener = null;
+      this.parent.w = this.parent.maxW;
+      this.parent.h = this.parent.maxH;
     }
-    this.graphics.clear().beginFill(this.parent.circleColor).drawCircle(0, 0, this.parent.radius);
+    this.graphics.clear().beginFill(this.parent.rectColor).drawRoundRect(-this.parent.w/2, -this.parent.h/2, this.parent.w, this.parent.h, this.parent.radius);
     // this.parent.updateCache();
   };
   p.handleMouseUp = function() {
     // console.log(this.parent.radius);
-    this.parent.radius -= this.parent.rampSpace;
-    if (this.parent.radius <= this.parent.minRadius) {
-      this.off("tick", this.parent.circleEventListener);
-      this.parent.circleEventListener = null;
-      this.parent.radius = this.parent.minRadius;
+    this.parent.w -= this.parent.rampSpaceW;
+    this.parent.H -= this.parent.rampSpaceH;
+    if (this.parent.w <= this.parent.minW || this.parent.h <= this.parent.minH) {
+      this.off("tick", this.parent.rRectEventListener);
+      this.parent.rRectEventListener = null;
+      this.parent.w = this.parent.minW;
+      this.parent.h = this.parent.minH;
     }
-    this.graphics.clear().beginFill(this.parent.circleColor).drawCircle(0, 0, this.parent.radius);
+    this.graphics.clear().beginFill(this.parent.rectColor).drawRoundRect(-this.parent.w/2, -this.parent.h/2, this.parent.w, this.parent.h, this.parent.radius);
     // this.parent.updateCache();
   };
-
-
 
   window.Vertex = createjs.promote(Vertex, "Container");
 }());
--- scripts/main.js
+++ scripts/main.js
@@ -16,54 +16,54 @@ createjs.Touch.enable(stage);
 const bg = new createjs.Shape();
 bg.graphics.beginFill('white').drawRect(0, 0, stage.canvas.width, stage.canvas.height);
 
-const circle1 = new Vertex(stage, "IndianRed", "IndianRed", 20, 23);
-circle1.x = stage.canvas.width/3;
-circle1.y = stage.canvas.height/3;
-circle1.snapToPixel = true;
-circle1.on("mousedown", function(evt) {
+const rRect1 = new Vertex(stage, 30, 50, "IndianRed", "IndianRed", 5);
+rRect1.x = stage.canvas.width/3;
+rRect1.y = stage.canvas.height/3;
+rRect1.snapToPixel = true;
+rRect1.on("mousedown", function(evt) {
   this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
 });
 
-circle1.on("pressmove", function (evt) {
+rRect1.on("pressmove", function (evt) {
   this.x = evt.stageX + this.offset.x;
   this.y = evt.stageY + this.offset.y;
   // indicate that the stage should be updated on the next tick:
 });
 
-circle1.on("pressup", function(evt) {
-  arcId['cp1x'] = circle1.x
-  arcId['cp1y'] = circle2.y
-  arcId['cp2x'] = circle1.x
-  arcId['cp2y'] = circle2.y
+rRect1.on("pressup", function(evt) {
+  arcId['cp1x'] = rRect1.x
+  arcId['cp1y'] = rRect2.y
+  arcId['cp2x'] = rRect1.x
+  arcId['cp2y'] = rRect2.y
 })
 
-const circle2 = new Vertex(stage, "Gray", "DarkGrey", 10, 12);
-circle2.x = stage.canvas.width/2;
-circle2.y = stage.canvas.height/2;
+const rRect2 = new Vertex(stage, 80, 50, "Gray", "DarkGrey", 5, 85, 55);
+rRect2.x = stage.canvas.width/2;
+rRect2.y = stage.canvas.height/2;
 
-circle2.on("mousedown", function(evt) {
+rRect2.on("mousedown", function(evt) {
   this.offset = {x: this.x - evt.stageX, y: this.y - evt.stageY};
 });
 
-circle2.on("pressmove", function (evt) {
+rRect2.on("pressmove", function (evt) {
   this.x = evt.stageX + this.offset.x;
   this.y = evt.stageY + this.offset.y;
 });
 
-circle2.on("pressup", function(evt) {
-  arcId['cp1x'] = circle1.x
-  arcId['cp1y'] = circle2.y
-  arcId['cp2x'] = circle1.x
-  arcId['cp2y'] = circle2.y
+rRect2.on("pressup", function(evt) {
+  arcId['cp1x'] = rRect1.x
+  arcId['cp1y'] = rRect2.y
+  arcId['cp2x'] = rRect1.x
+  arcId['cp2y'] = rRect2.y
 })
 
 var arc1 = new createjs.Shape();
-arc1.graphics.setStrokeStyle(4).setStrokeDash([15, 5], dash).beginStroke("DeepSkyBlue").moveTo(circle1.x, circle1.y).bezierCurveTo(arcId['cp1x'], arcId['cp1y'], arcId['cp2x'], arcId['cp2y'], circle2.x, circle2.y);
+arc1.graphics.setStrokeStyle(4).setStrokeDash([15, 5], dash).beginStroke("DeepSkyBlue").moveTo(rRect1.x, rRect1.y).bezierCurveTo(arcId['cp1x'], arcId['cp1y'], arcId['cp2x'], arcId['cp2y'], rRect2.x, rRect2.y);
 
 stage.addChild(bg);
 stage.addChild(arc1);
-stage.addChild(circle1);
-stage.addChild(circle2);
+stage.addChild(rRect1);
+stage.addChild(rRect2);
 createjs.Ticker.timingMode = createjs.Ticker.RAF;
 stage.update();
 
@@ -73,7 +73,7 @@ createjs.Ticker.on("tick", function() {
 
 arc1.on("tick", function() {
   dash = (dash+1)%20;
-  arc1.graphics.clear().setStrokeStyle(4).setStrokeDash([15, 5], dash).beginStroke("DeepSkyBlue").moveTo(circle1.x, circle1.y).bezierCurveTo(arcId['cp1x'], arcId['cp1y'], arcId['cp2x'], arcId['cp2y'], circle2.x, circle2.y);
+  arc1.graphics.clear().setStrokeStyle(4).setStrokeDash([15, 5], dash).beginStroke("DeepSkyBlue").moveTo(rRect1.x, rRect1.y).bezierCurveTo(arcId['cp1x'], arcId['cp1y'], arcId['cp2x'], arcId['cp2y'], rRect2.x, rRect2.y);
 });
 
 window.changeArc = function(id, value) {
